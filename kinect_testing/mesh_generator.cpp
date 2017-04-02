af157f467e9a2089ceda0b15f9b256bdcc78115f
MiroslavKohut <miros.kohutos@gmail.com>
2017-04-02 21:00:24 +0200

    rotations fixed

--- kinect_testing/mesh_generator.cpp
+++ kinect_testing/mesh_generator.cpp
@@ -56,18 +56,21 @@ public:
     Meshgen();
     ~Meshgen();
     void transformPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target target);
-    void transformOriginPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target_origin target_origin);
-    void generatePointCloud(const cv::Mat & rgbMat, const cv::Mat & depthMat, pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud,int id);
+    void transformOriginPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target target_origin);
+    void generatePointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & final_cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud);
 
-private:
-    const struct Target target_10 ={700,0,200,0,-0.707106781,0.707106781,0};
-    const struct Target target_20 ={1049.999698851,-350.00005995,549.999355294,0.499999609,0.000000171,0.707107214,-0.499999778};
-    const struct Target target_30 ={1400,-700,200,0,0,1,0};
-    const struct Target target_40 ={700,-1400,200,0,0.707106781,0.707106781,0};
-    const struct Target target_50 ={0,-700,200,0,1,0,0};
-    const struct Target target_60 ={700,-600,900,0.5,0.5,0.5,-0.5};
-    const struct Target target_70 ={1050,-1050,550,0.353553391,0.353553389,0.853553391,-0.14644661};
-    const struct Target target_80 ={350,-1050,550,0.14644661,0.853553391,0.353553389,-0.353553391};
+    const struct Target target_10 ={0.700,0,0.200,0,0,0,0};
+    const struct Target target_20 ={1.049999698851,-0.35000005995,0.549999355294,0,0,0,0};
+    const struct Target target_30 ={1.400,-0.7,0.2,0,0,0,0};
+
+    const struct Target target_40 ={0.700,-1.400,0.200,0,0,0,0}; // musim ydat kladne
+
+    const struct Target target_50 ={0.0,-0.700,0.200,0,0,0,0};
+
+    //const struct Target target_60 ={0.700,-0.600,0.900,0,0,0,0};
+
+    const struct Target target_70 ={1.050,-1.050,0.550,0,0,0,0};
+    const struct Target target_80 ={0.350,-1.050,0.550,0,0,0,0};
 
 
     int depth_idx_f;
@@ -80,44 +83,68 @@ Meshgen::Meshgen(){
 }
 void Meshgen::transformPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target target) {
 
+
+    Target fixed_target;
+
+    fixed_target.tx = target.tx;
+    fixed_target.ty = target.tz;
+    fixed_target.tz = -target.ty;
+
     Eigen::Affine3f transform_x = Eigen::Affine3f::Identity();
     Eigen::Affine3f transform_y = Eigen::Affine3f::Identity();
     Eigen::Affine3f transform_z = Eigen::Affine3f::Identity();
+
+    Eigen::Affine3f transform_x_y_z = Eigen::Affine3f::Identity();
+
     // Define a translation of 2.5 meters on the x axis.
+    //std::cout << transform_x_y_z.matrix() << std::endl;
+    transform_x_y_z.translation() << fixed_target.tx, fixed_target.ty, fixed_target.tz;
+    std::cout << transform_x_y_z.matrix() << std::endl;
+
+    pcl::transformPointCloud (*pointcloud, *pointcloud, transform_x_y_z);
+
     transform_x.rotate (Eigen::AngleAxisf (target.rx, Eigen::Vector3f::UnitX()));
+
     transform_y.rotate (Eigen::AngleAxisf (target.ry, Eigen::Vector3f::UnitY()));
 
-    transform_z.translation() << target.tx, target.ty, target.tz;
     transform_z.rotate (Eigen::AngleAxisf (target.rz, Eigen::Vector3f::UnitZ()));
 
 
     //std::cout << transform_x.matrix() << std::endl;
     pcl::transformPointCloud (*pointcloud, *pointcloud, transform_x);
 
-
     //std::cout << transform_y.matrix() << std::endl;
     pcl::transformPointCloud (*pointcloud, *pointcloud, transform_y);
-
     // You can either apply transform_1 or transform_2; they are the same
     //std::cout << transform_z.matrix() << std::endl;
     pcl::transformPointCloud (*pointcloud, *pointcloud, transform_z);
 }
 
-void Meshgen::transformOriginPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target_origin target_origin) {
+void Meshgen::transformOriginPointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud, Target target_origin) {
 
     //TODO Implement trasnformation of pointcloud sensor origin;
-    pointcloud->sensor_orientation_ = target_origin.orientation;
-    pointcloud->sensor_origin_ = target_origin.origin;
+    Eigen::Quaternionf rotation(target_origin.rw,target_origin.rx,target_origin.ry,target_origin.rz);
+    Eigen::Vector4f translation(target_origin.tx,target_origin.ty,target_origin.tz,0);
 
-
+    cout << "tocim" << endl;
+    pointcloud->sensor_orientation_ = rotation;
+    pointcloud->sensor_origin_ = translation;
 }
 
 
-void Meshgen::generatePointCloud(const cv::Mat & rgbMat, const cv::Mat & depthMat, pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud,int id) {
-    pointcloud->width = rgbMat.cols;
-    pointcloud->height = rgbMat.rows;
-    pointcloud->points.resize (pointcloud->height * pointcloud->width);
+void Meshgen::generatePointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr & final_cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr & pointcloud) {
+    //pointcloud->width = rgbMat.cols;
+    //pointcloud->height = rgbMat.rows;
+    //pointcloud->points.resize (pointcloud->height * pointcloud->width);
 
+    //const struct Target target_help ={0,0,0,0,1.57,0,0};
+
+    //transformPointCloud(pointcloud,target_help);
+    for (int i; i < pointcloud->points.size();i++){
+        final_cloud->points.push_back(pointcloud->points[i]);
+    }
+
+/*
     float  focalLength = 525;
     float centerX = 0;
     float centerY = 0;
@@ -147,37 +174,6 @@ void Meshgen::generatePointCloud(const cv::Mat & rgbMat, const cv::Mat & depthMa
 
 
         }
-    }
-    //basic data transform
-    Target target={0,0,0,-M_PI_2,0,-M_PI_2};
-    this->transformPointCloud(pointcloud,target);
-/*
-    switch(id){
-        case 10 :
-            this->transformPointCloud(pointcloud,this->target_10);
-            break;
-        case 20  :
-            this->transformPointCloud(pointcloud,this->target_20);
-            break;
-        case 30 :
-            this->transformPointCloud(pointcloud,this->target_30);
-            break;
-        case 40  :
-            this->transformPointCloud(pointcloud,this->target_40);
-            break;
-        case 50 :
-            this->transformPointCloud(pointcloud,this->target_50);
-            break;
-        case 60  :
-            this->transformPointCloud(pointcloud,this->target_60);
-            break;
-        case 70 :
-            this->transformPointCloud(pointcloud,this->target_70);
-            break;
-        case 80  :
-            this->transformPointCloud(pointcloud,this->target_80);
-            break;
-        default :;
     }*/
 
 }
@@ -206,19 +202,68 @@ int main(int argc, char **argv) {
 
     //namedWindow("filtered_depth",CV_WINDOW_AUTOSIZE);
     //namedWindow("rgb_show",CV_WINDOW_AUTOSIZE);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr mesh_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::visualization::CloudViewer viewer("Cloud Viewer");
 
-    for (int i = 10;i<=80;i = i+10){
+    for (int i = 10;i<=60;i = i+10){
 
+
+
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
         Reader.read("/home/mirec/Documents/Capture_data_kinect/test/target_"+to_string(i)+".ply", *cloud);
-        viewer.showCloud(cloud);
+        Target target = {0, 0, 0, 0, 3.14, 0, 0};
 
-        //cvWaitKey(5000);
+       switch(i){
+            case 10 :
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_10);
+                break;
+            case 20  :
+                continue;
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_20);
+                break;
+            case 30 :
+                target = {0, 0, 0, 0, -1.57, 0, 0};
+                mesh_generator->transformPointCloud(cloud,target);
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_30);
+                break;
+            case 40  :
+                target = {0, 0, 0, 0, 3.14, 0, 0};
+                mesh_generator->transformPointCloud(cloud,target);
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_40);
+                break;
+            case 50 :
+                target = {0, 0, 0, 0, 1.57, 0, 0};
+                mesh_generator->transformPointCloud(cloud,target);
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_50);
+                break;
+            case 60  :
+                continue;
+                target = {0, 0, 0, -1.570, 0, 0, 0};
+                mesh_generator->transformPointCloud(cloud,target);
+                target ={0.700,-0.900,-0.6,0,0,0,0};
+                mesh_generator->transformPointCloud(cloud,target);
+                break;
+            case 70 :
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_70);
+                break;
+            case 80  :
+                mesh_generator->transformPointCloud(cloud,mesh_generator->target_80);
+                break;
+            default :;
+        }
+        //pcl::io::savePLYFileBinary("novy_cloud_"+to_string(i)+".ply",*cloud);
+        mesh_generator->generatePointCloud(mesh_cloud,cloud);
+        viewer.showCloud(mesh_cloud);
+        //sleep(10);
     }
+    pcl::io::savePLYFileBinary("novy_cloud.ply",*mesh_cloud);
+    sleep(50);
+    /*viewer.showCloud(mesh_cloud);
+    sleep(10);
+    cvWaitKey(5000000);*/
 
-    Reader.read("/home/mirec/Documents/Capture_data_kinect/test/target_"+to_string(10)+".ply", *cloud);
-    pcl::io::savePLYFileBinary("novy_cloud.ply",*cloud);
+    //pcl::io::savePLYFileBinary("novy_cloud.ply",*mesh_cloud);
 
         /*if(data_load->readImageDepth("depth_target_"+file_name,depthMat)) {
 
